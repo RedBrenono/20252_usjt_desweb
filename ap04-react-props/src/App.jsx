efab2fe2f4df8b1a36d8d25dd8cbfceaec882a5c
Breno Henrique <brenoruiz2004@gmail.com>
2025-09-04 21:50:18 -0300

    termina em casa

---
+++ ap04-react-props/src/App.jsx
@@ -0,0 +1,24 @@
+const App = () => {
+    return(
+        <div className="container bg-primary p-5">
+            <div className="row">
+                <div className="col-12">
+                    <i className="fa-hippo fa-2x fa-solid"></i>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12 col-md-6 col-lg-3">
+                     <div className="card">
+                        <div className="card-header"></div>
+                            
+                     </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-3">
+                    <div>  </div>
+                </div>    
+            </div>
+        </div>
+    )
+}
+
+export default App
